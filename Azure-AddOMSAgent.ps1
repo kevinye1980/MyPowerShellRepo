74ceaa7683db1800e25b40518f2a9af94554dbbf
Kevin Ye <chunye@live.com>
2017-08-02 10:48:32 +0800

    Added powershell help info

--- Azure-AddOMSAgent.ps1
+++ Azure-AddOMSAgent.ps1
@@ -1,7 +1,53 @@
-﻿
-# Set subscription name
-$subscriptionName = "<your subscription name>"
+<# 
+	.SYNOPSIS
+	This sciprt is used to install OMS agent on Azure VMs in a particular resource group. 
+  
+	.DESCRIPTION
+	This sciprt is used to install OMS agent on Azure VMs in a particular resource group. 
+  Azure VMs must be in running state. 
+  
+  
+	.PARAMETER subscriptionName
+	Your Azure subscription name.
+	.PARAMETER workspaceName
+  Your OMS workspace name
+  .PARAMETER VMresourcegroup
+  The Azure resource group which contains VMs you want to enable OMS agent
+  
 
+	.INPUTS
+	Nothing
+
+	.OUTPUTS
+	Nothing
+	
+	.NOTES
+    Author: Kevin Ye
+
+    
+	
+	DISCLAIMER:
+	This sample script is not supported under any Microsoft standard support program or service. This sample
+	script is provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties
+	including, without limitation, any implied warranties of merchantability or of fitness for a particular
+	purpose. The entire risk arising out of the use or performance of this sample script and documentation
+	remains with you. In no event shall Microsoft, its authors, or anyone else involved in the creation,
+	production, or delivery of this script be liable for any damages whatsoever (including, without limitation,
+	damages for loss of business profits, business interruption, loss of business information, or other
+	pecuniary loss) arising out of the use of or inability to use this sample script or documentation, even
+	if Microsoft has been advised of the possibility of such damages.
+	
+#>
+
+param(	
+      [Parameter(Mandatory=$true)]
+			[String]$subscriptionName,
+      [Parameter(Mandatory=$true)]
+			[String]$workspaceName,
+      [Parameter(Mandatory=$true)]
+			[String]$VMresourcegroup
+			)
+      
 # Login to the specified subscription
 Try{
   $subscription = Get-AzureRMSubscription -SubscriptionName $subscriptionName
@@ -11,14 +57,8 @@ Try{
   $subscription = Get-AzureRMSubscription -SubscriptionName $subscriptionName
 }
 
-# Set OMS workspace
-$workspaceName = "<Your OMS workspace name>"
-# Set the resource group which contains the target VMs
-$VMresourcegroup = "<Your resource group name>"
-
-
+# Connect to OMS workspace
 Try{
-    # Connect to OMS workspace
     $workspace = (Get-AzureRmOperationalInsightsWorkspace).Where({$_.Name -eq $workspaceName})
 
     if ($workspace -eq $null)
